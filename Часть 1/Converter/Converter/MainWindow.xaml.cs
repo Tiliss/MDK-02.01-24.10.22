be6f83e951988c963a54ec908b84b3a650299a53
Surganov Konstantin <105750598+Tiliss@users.noreply.github.com>
2022-10-24 18:44:59 +0500

    Add files via upload

---
+++ Часть 1/Converter/Converter/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Converter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        class Base64Converter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is string)
+                {
+                    return Base64Encode((string)value);
+                }
+                return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                try
+                {
+                    if (value is string)
+                    {
+                        return Base64Decode((string)value);
+                    }
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+                return value;
+            }
+
+            public static string Base64Encode(string text)
+            {
+                return System.Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+            }
+
+            public static string Base64Decode(string text)
+            {
+                return Encoding.UTF8.GetString(System.Convert.FromBase64String(text));
+            }
+        }
+    }
+}
